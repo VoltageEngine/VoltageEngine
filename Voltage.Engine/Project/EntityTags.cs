74bf6871a582fc0ac147890239c6762b139fd924
GiorgosTsolakidis <rossakis@hotmail.com>
2026-05-18 01:44:18 +0300

    Separated physics and render layers into static classes

---
+++ Voltage.Engine/Project/EntityTags.cs
@@ -0,0 +1,27 @@
+﻿
+namespace Voltage.Project
+{
+	public class EntityTags
+	{
+		/// <summary>
+		/// Gets the entity tag value by its name.
+		/// Returns 0 if not found.
+		/// </summary>
+		public int GetEntityTag(string tagName)
+		{
+			if (ProjectSettings.Instance.Entities.EntityTags.TryGetValue(tagName, out var tagValue))
+				return tagValue;
+
+			Debug.Error($"[ProjectSettings] Entity tag '{tagName}' not found. Did you add it in Project Settings?");
+			return 0;
+		}
+
+		/// <summary>
+		/// Tries to get the entity tag value by its name.
+		/// </summary>
+		public bool TryGetEntityTag(string tagName, out int tagValue)
+		{
+			return ProjectSettings.Instance.Entities.EntityTags.TryGetValue(tagName, out tagValue);
+		}
+	}
+}
