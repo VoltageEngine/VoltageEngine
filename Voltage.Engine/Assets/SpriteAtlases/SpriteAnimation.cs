92b1235f6f186a0ee1c37865664ff8123a6b3320
GiorgosTsolakidis <rossakis@hotmail.com>
2026-06-05 20:19:55 +0300

    Added animation support for particle emitters + fixed game build error

--- Voltage.Engine/Assets/SpriteAtlases/SpriteAnimation.cs
+++ Voltage.Engine/Assets/SpriteAtlases/SpriteAnimation.cs
@@ -21,18 +21,4 @@ public class SpriteAnimation
 		Sprites = sprites;
 		FrameRates = frameRates;
 	}
-
-	// public SpriteAnimation Clone()
-	// {
-	// 	// Deep copy Sprites array (clone each Sprite)
-	// 	var spritesClone = new Sprite[Sprites.Length];
-	// 	for (int i = 0; i < Sprites.Length; i++)
-	// 		spritesClone[i] = Sprites[i]?.Clone();
-	//
-	// 	// Deep copy FrameRates array
-	// 	var frameRatesClone = new float[FrameRates.Length];
-	// 	Array.Copy(FrameRates, frameRatesClone, FrameRates.Length);
-	//
-	// 	return new SpriteAnimation(spritesClone, frameRatesClone);
-	// }
 }
